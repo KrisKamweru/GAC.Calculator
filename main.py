bf588478c06a886323f99325a65030e886b5da15
KrisKamweru <kriskamweru@gmail.com>
2019-11-08 19:05:04 +0300

    Added class to hold GP Values, and choice field to display GP brackets

--- main.py
+++ main.py
@@ -1,14 +1,35 @@
 import wx
 
+#declare classes that will hold static values
+class variables_5v5_NoShips:
+    gpValues = ['0 - 599.9k', '600k - 799.9k', '800k - 999.9k', '1M - 1.29M', '1.3M - 1.59M', '1.6M - 1.99M', '2M - 2.49M', '2.5M - 3.09M', '3.1M - 3.79M', '3.8M - 4.49M', '4.5M+']
+    gpDivisions = [11, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1]
+    gacDefences = [4, 4, 4, 4, 4, 5, 5, 6, 6, 8, 8]
+
 class MyFrame(wx.Frame):
     def __init__(self):
-        super().__init__(parent=None, title='Hello World')
+        super().__init__(parent=None, title='GAC Calculator')
+
+        #declare panel
         panel = wx.Panel(self)
+        #declare Box Sizer
+        my_sizer = wx.BoxSizer(wx.VERTICAL)
+        #declare calculate button widget
+        calculateBtn = wx.Button(panel, label='Calculate', pos=(5, 55))
+        #declare GP choice widget
+        self.gpChoice = wx.Choice(panel, choices = variables_5v5_NoShips.gpValues)
 
-        self.text_ctrl = wx.TextCtrl(panel, pos=(5, 5))
-        my_btn = wx.Button(panel, label='Press Me', pos=(5, 55))
+        #add widgets to Sizer
+        my_sizer.Add(self.gpChoice, 0, wx.ALL | wx.EXPAND, 5)
+        my_sizer.Add(calculateBtn, 0, wx.ALL | wx.CENTER, 5)
 
+        calculateBtn.Bind(wx.EVT_BUTTON, self.on_press)
+
+        panel.SetSizer(my_sizer)
         self.Show()
+
+    def on_press(self, event):
+        print("In Development")
 
 if __name__ == '__main__':
     app = wx.App()
