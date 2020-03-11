da5c040710ef9228ee080cff046f286266881b01
KrisKamweru <kriskamweru@gmail.com>
2020-03-11 16:18:19 +0300

    Update main.py
    Added code to get set defences, and the validation for that.
    Added a bunch of comments as well

--- main.py
+++ main.py
@@ -38,6 +38,11 @@ class variables_3v3:
 	gacSquadDefences = [4, 6, 8, 10, 11]
 	gacShipDefences = 1
 
+#
+#
+#Begin program (apologies for my C/Java way of thinking)
+#
+#
 
 #Get squad size for GAC
 squadSizeChoice = input("Type 1 for 5v5, or 2 for 3v3: ")
@@ -56,22 +61,87 @@ while shipsChoice not in ["1", "2"]:
 	shipsChoice = input("Type 1 if ships are present, or 2 if they aren't: ")
 
 #Get GP Division
-if((squadSizeChoice == "1") and (shipsChoice == "2")):
+if((squadSizeChoice == "1") and (shipsChoice == "2")): #5v5 - No Ships
 	print("Choose your GP division: ")
 	for x in range(len(variables_5v5.gpValues)):
 		print(x+1, " ",  variables_5v5.gpValues[x])
 	gpChoice = int(input(": "))
 
-elif((squadSizeChoice == "2") and (shipsChoice == "2")):
+elif((squadSizeChoice == "2") and (shipsChoice == "2")): #3v3 - No Ships
 	print("Choose your GP division: ")
 	for x in range(len(variables_3v3.gpValues)):
 		print(x+1, " ",  variables_3v3.gpValues[x])
 	gpChoice = int(input(": "))
 
-elif((squadSizeChoice == "1") and (shipsChoice == "1")):
+elif((squadSizeChoice == "1") and (shipsChoice == "1")): #5v5 - With Ships
 	print("stuff")
 
-elif((squadSizeChoice == "2") and (shipsChoice == "1")):
+elif((squadSizeChoice == "2") and (shipsChoice == "1")): #3v3 - With Ships
 	print("stuff")
+
+else:
+	print("Something has gone horribly wrong \n\n\n\n\n\n\n") #if the code executes this, something really has gone horribly wrong with my logic :P
 
 print(variables_5v5.gpValues[gpChoice - 1])
+
+#Check if all defences were set
+if((squadSizeChoice == "1") and (shipsChoice == "2")): #5v5 - No Ships
+	validation = input("Did you set all " + str(variables_5v5.gacSquadDefences[gpChoice - 1]) +  " defences? (y/n): ")
+
+elif((squadSizeChoice == "2") and (shipsChoice == "2")): #3v3 - No Ships
+	validation = input("Did you set all " + str(variables_3v3.gacSquadDefences[gpChoice - 1]) +  " defences? (y/n): ")
+
+elif((squadSizeChoice == "1") and (shipsChoice == "1")): #5v5 - With Ships
+	validation = input("Did you set all " + str(variables_5v5.gacSquadDefences[gpChoice - 1]) +  " squad defences and all " + str(variables_5v5.gacShipDefences) + " ship defences? (y/n): ")
+
+elif((squadSizeChoice == "2") and (shipsChoice == "1")): #3v3 - With Ships
+		validation = input("Did you set all " + str(variables_3v3.gacSquadDefences[gpChoice - 1]) +  " squad defences and all " + str(variables_3v3.gacShipDefences) + " ship defences? (y/n): ")
+
+else:
+	print("Something has gone horribly wrong \n\n\n\n\n\n\n") #if the code executes this, something really has gone horribly wrong with my logic :P
+
+#Validation check for 
+while validation not in ["N", "n", "Y", "y"]:
+	print("Input was invalid. Please try again")
+	if((squadSizeChoice == "1") and (shipsChoice == "2")): #5v5 - No Ships
+		validation = input("Did you set all " + str(variables_5v5.gacSquadDefences[gpChoice - 1]) +  " defences? (y/n): ")
+
+	elif((squadSizeChoice == "2") and (shipsChoice == "2")): #3v3 - No Ships
+		validation = input("Did you set all " + str(variables_3v3.gacSquadDefences[gpChoice - 1]) +  " defences? (y/n): ")
+
+	elif((squadSizeChoice == "1") and (shipsChoice == "1")): #5v5 - With Ships
+		validation = input("Did you set all " + str(variables_5v5.gacSquadDefences[gpChoice - 1]) +  " squad defences and all " + str(variables_5v5.gacShipDefences) + " ship defences? (y/n): ")
+
+	elif((squadSizeChoice == "2") and (shipsChoice == "1")): #3v3 - With Ships
+			validation = input("Did you set all " + str(variables_3v3.gacSquadDefences[gpChoice - 1]) +  " squad defences and all " + str(variables_3v3.gacShipDefences) + " ship defences? (y/n): ")
+
+	else:
+		print("Something has gone horribly wrong \n\n\n\n\n\n\n") #if the code executes this, something really has gone horribly wrong with my logic :P
+
+
+#If not all defences set
+if validation in ["N", "n"]:
+	if((squadSizeChoice == "1") and (shipsChoice == "2")): #5v5 - No Ships
+		setDefences = int(input("How many defences did you set?: "))
+		while(setDefences > variables_5v5.gacSquadDefences[gpChoice - 1]):
+			print("Invalid input, please try again \n")
+			setDefences = int(input("How many defences did you set?: "))
+		
+		if(setDefences == variables_5v5.gacSquadDefences[gpChoice - 1]):
+			print("Looks like you set all your defences.")
+			validation = input("Did you really set all your defences? (y/n): ")
+
+	elif((squadSizeChoice == "2") and (shipsChoice == "2")): #3v3 - No Ships
+		pass
+	
+	elif((squadSizeChoice == "1") and (shipsChoice == "1")): #5v5 - With Ships
+		pass
+
+	elif((squadSizeChoice == "2") and (shipsChoice == "1")): #3v3 - With Ships
+		pass
+
+elif validation in ["Y", "y"]:
+	pass
+
+else:
+	print("Something has gone horribly wrong \n\n\n\n\n\n\n") #if the code executes this, something really has gone horribly wrong with my logic :P
